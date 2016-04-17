a588d7527f89d5e9fe887fbd5c3bf361c801254e
Thomas Watson Steen <w@tson.dk>
2016-04-17 23:58:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+# airplay-protocol
+
+A low level protocol wrapper on top of the AirPlay HTTP API.
+
+[![Build status](https://travis-ci.org/watson/airplay-protocol.svg?branch=master)](https://travis-ci.org/watson/airplay-protocol)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
+## Installation
+
+```
+npm install airplay-protocol --save
+```
+
+## API
+
+### `new AirPlay(ip[, port])`
+
+Initiate a connection to a specific AirPlay server given an IP address
+and port. If no port is given, the default port 7000 is used.
+
+Returns an instance of the AirPlay object.
+
+```js
+var AirPlay = require('airplay-protocol')
+
+var airplay = new AirPlay('192.168.0.42', 7000)
+```
+
+### Event: `state`
+
+```js
+function (state) {}
+```
+
+Emitted by the AirPlay server every time the state of the playback
+changes.
+
+Possible states: `loading`, `playing`, `paused` or `stopped`.
+
+### `airplay.serverInfo(callback)`
+
+Get the AirPlay server info.
+
+Calls the callback with the plist result as the second argument or
+optionally an error object as the first argument.
+
+### `airplay.play(url[, position][, callback])`
+
+Start video playback.
+
+Arguments:
+
+- `url` - The URL to play
+- `position` - Optional start position. A floating point number between
+  `0` and `1` where `0` represents the begining of the video and `1` the
+  end. Defaults to `0`
+- `callback` - Optional callback. Will be called when the play request
+  have been processed by the AirPlay server. An error object may be
+  given as the first argument
+
+### `airplay.scrub(callback)`
+
+Retrieve the current playback position.
+
+Arguments:
+
+- `callback` - Will be called with the current playback position as the
+  second argument. An error object may be given as the first argument
+
+### `airplay.scrub(position[, callback])`
+
+Seek to an arbitrary location in the video.
+
+Arguments:
+
+- `position` - A float value representing the location in seconds
+- `callback` - Optional callback. Will be called when the scrub request
+  have been processed by the AirPlay server. An error object may be
+  given as the first argument
+
+### `airplay.rate(speed[, callback])`
+
+Change the playback rate.
+
+Arguments:
+
+- `speed` - A float value representing the playback rate: 0 is paused, 1
+  is playing at the normal speed
+- `callback` - Optional callback. Will be called when the scrub request
+  have been processed by the AirPlay server. An error object may be
+  given as the first argument
+
+### `airplay.stop([callback])`
+
+Stop playback.
+
+Arguments:
+
+- `callback` - Optional callback. Will be called when the stop request
+  have been processed by the AirPlay server. An error object may be
+  given as the first argument
+
+### `airplay.playbackInfo(callback)`
+
+Retrieve playback informations such as position, duration, rate,
+buffering status and more.
+
+Arguments:
+
+- `callback` - Will be called with a playback-info object as the second
+  argument. An error object may be given as the first argument
+
+### `airplay.property(name, callback)`
+
+Get playback property.
+
+Arguments:
+
+- `name` - The name of the property to get
+- `callback` - Will be called with the property value as the second
+  argument. An error object may be given as the first argument
+
+### `airplay.property(name, value[, callback])`
+
+Set playback property.
+
+Arguments:
+
+- `name` - The name of the property to set
+- `value` - The plist object to set
+- `callback` - Optional callback. Will be called when the set-property
+  request have been processed by the AirPlay server. An error object may
+  be given as the first argument
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,172 @@
+'use strict'
+
+var util = require('util')
+var EventEmitter = require('events').EventEmitter
+var reverseHttp = require('reverse-http')
+var request = require('request')
+var plist = require('plist')
+var bplist = {
+  encode: require('bplist-creator'),
+  decode: require('bplist-parser').parseBuffer
+}
+
+var noop = function () {}
+
+module.exports = AirPlay
+
+util.inherits(AirPlay, EventEmitter)
+
+function AirPlay (host, port) {
+  if (!(this instanceof AirPlay)) return new AirPlay(host, port)
+  if (!port) port = 7000
+
+  EventEmitter.call(this)
+
+  this.host = host
+  this.port = port
+
+  // TODO: It might be smart to wait starting this until we play and then close
+  // it when we're done playing
+  this._startReverse()
+}
+
+AirPlay.prototype._startReverse = function () {
+  var self = this
+
+  this._rserver = reverseHttp({ hostname: this.host, port: this.port, path: '/reverse' }, function (req, res) {
+    if (req.method !== 'POST' || req.url !== '/event') {
+      // TODO: Maybe we should just accept it silently?
+      res.statusCode = 404
+      res.end()
+      return
+    }
+
+    var buffers = []
+    req.on('data', buffers.push.bind(buffers))
+    req.on('end', function () {
+      res.end()
+
+      var data = Buffer.concat(buffers)
+
+      switch (req.headers['content-type']) {
+        case 'text/x-apple-plist+xml':
+        case 'application/x-apple-plist':
+          data = plist.parse(data.toString())
+          break
+      }
+
+      self.emit('state', data)
+    })
+  })
+}
+
+AirPlay.prototype.close = function (cb) {
+  this._rserver.close(cb)
+}
+
+AirPlay.prototype.serverInfo = function serverInfo (cb) {
+  this._request('GET', '/server-info', cb)
+}
+
+AirPlay.prototype.play = function play (url, position, cb) {
+  if (typeof position === 'function') return this.play(url, 0, position)
+
+  var body = 'Content-Location: ' + url + '\n' +
+             'Start-Position: ' + position + '\n'
+
+  this._request('POST', '/play', body, cb || noop)
+}
+
+AirPlay.prototype.scrub = function scrub (position, cb) {
+  if (typeof position === 'function') return this.scrub(null, position)
+
+  var method, path
+  if (position === null) {
+    method = 'GET'
+    path = '/scrub'
+  } else {
+    method = 'POST'
+    path = '/scrub?position=' + position
+  }
+
+  this._request(method, path, cb || noop)
+}
+
+AirPlay.prototype.rate = function rate (speed, cb) {
+  this._request('POST', '/rate?value=' + speed, cb || noop)
+}
+
+AirPlay.prototype.stop = function stop (cb) {
+  this._request('POST', '/stop', cb || noop)
+}
+
+AirPlay.prototype.playbackInfo = function playbackInfo (cb) {
+  this._request('GET', '/playback-info', cb)
+}
+
+AirPlay.prototype.property = function property (name, value, cb) {
+  if (typeof value === 'function') return this.property(name, null, value)
+
+  var method, path
+  if (value === null) {
+    method = 'POST'
+    path = '/getProperty?' + name
+  } else {
+    method = 'PUT'
+    path = '/setProperty?' + name
+  }
+
+  this._request(method, path, value, cb)
+}
+
+AirPlay.prototype._request = function _request (method, path, body, cb) {
+  if (typeof body === 'function') return this._request(method, path, null, body)
+
+  var opts = this._reqOpts(method, path, body)
+
+  request(opts, function (err, res, body) {
+    if (err) return cb(err, res, body)
+    if (res.statusCode !== 200) err = new Error('Unexpected response from Apple TV: ' + res.statusCode)
+
+    switch (res.headers['content-type']) {
+      case 'application/x-apple-binary-plist':
+        body = bplist.decode(body)[0]
+        break
+      case 'text/x-apple-plist+xml':
+        body = plist.parse(body.toString())
+        break
+      case 'text/parameters':
+        body = body.toString().trim().split('\n').reduce(function (body, line) {
+          line = line.split(': ')
+          // TODO: For now it's only floats, but it might be better to not expect that
+          body[line[0]] = Number.parseFloat(line[1], 10)
+          return body
+        }, {})
+        break
+    }
+
+    cb(err, res, body)
+  })
+}
+
+AirPlay.prototype._reqOpts = function _reqOpts (method, path, body) {
+  var opts = {
+    method: method,
+    url: 'http://' + this.host + ':' + this.port + path,
+    headers: {
+      'User-Agent': 'iTunes/11.0.2'
+    },
+    encoding: null, // In case a binary plist is returned we don't want to convert it to a string
+    forever: true // The Apple TV will refuse to play if the play socket is closed
+  }
+
+  if (body && typeof body === 'object') {
+    opts.headers['Content-Type'] = 'application/x-apple-binary-plist'
+    opts.body = bplist.encode(body)
+  } else if (typeof body === 'string') {
+    opts.headers['Content-Type'] = 'text/parameters'
+    opts.body = body
+  }
+
+  return opts
+}
